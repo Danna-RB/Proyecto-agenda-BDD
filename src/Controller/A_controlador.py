c6fad2ab54ad83bbedec1ae7a6bc9cfc0eafc062
Danna Lizeth Rodriguez Bonilla <rodriguez021006@gmail.com>
2026-03-20 13:26:03 -0500

    Actualizar A_controlador.py

--- src/Controller/A_controlador.py
+++ src/Controller/A_controlador.py
@@ -2,35 +2,35 @@ from Model import A_modelo
 from View import A_vista  
 
 def iniciar ():
-    while True:  #ciclo infinito del programa
-        A_vista.Mostrarmenu()  #muestra menu
-        opcion = input("Seleccione opcion: ")  #pide opcion
+    while True: 
+        A_vista.Mostrarmenu() 
+        opcion = input("Seleccione opcion: ") 
         if opcion == "1":  #guardar contacto
-            datos = A_vista.Pedirdatos()  #pide datos a la vista
-            A_modelo.Guardar_Registro(*datos)  #guarda los datos en el modelo
+            datos = A_vista.Pedirdatos()
+            A_modelo.Guardar_Registro(*datos)
 
             print("Contacto guardado") 
         elif opcion == "2":  #buscar contacto
 
-            id = A_vista.Pedirid()  #pide ID
-            r = A_modelo.buscarRegistro(id)  #busca registro
-            A_vista.Mostrarcontacto(r)  #muestra resultado
+            id = A_vista.Pedirid()
+            r = A_modelo.buscarRegistro(id)
+            A_vista.Mostrarcontacto(r) 
         elif opcion == "3":  #eliminar contacto
 
-            id = A_vista.Pedirid()  #pide ID
-            eliminado = A_modelo.eliminarRegistros(id)  #elimina registro
+            id = A_vista.Pedirid() 
+            eliminado = A_modelo.eliminarRegistros(id)
 
-            if eliminado:  #si se eliminó
+            if eliminado:
                 print("Contacto eliminado")
             else:
                 print("No se encontró el contacto")
         elif opcion == "4": #mostrar todos
 
-            registros = A_modelo.Leer_Registros()  #obtiene registros
-            A_vista.Mostrarlista(registros)  #muestra lista
+            registros = A_modelo.Leer_Registros() 
+            A_vista.Mostrarlista(registros)
         elif opcion == "5":  #salir
 
-            print("Saliendo del programa")  #mensaje
-            break  #termina programa
+            print("Saliendo del programa")
+            break 
         else:
-            print("Opcion invalida")  #error
+            print("Opcion invalida")
